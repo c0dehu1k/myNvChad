ad0295dab8209368b7b80de84a997350c377cc96
Sunil Joshi <4852488+c0dehu1k@users.noreply.github.com>
2024-10-02 16:19:22 +0530

    ts_ls and foldings

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -3,4 +3,15 @@
  M.ui = {theme = 'catppuccin'}
  M.plugins = "custom.plugins"
  M.mappings = require "custom.mappings"
- return M
+
+local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+-- Use Treesitter for folding
+o.foldmethod = 'expr'
+o.foldexpr = 'nvim_treesitter#foldexpr()'
+
+-- Set a high foldlevel to prevent folding everything on startup
+o.foldlevel = 99
+o.foldenable = true -- Enable folding by default
+
+return M
--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -29,7 +29,7 @@ lspconfig.gopls.setup {
   },
 }
 
-lspconfig.tsserver.setup{
+lspconfig.ts_ls.setup{
   on_attach = on_attach,
   capabilities = capabilities,
   filetypes = { "html", "css", "js", "ts"},
